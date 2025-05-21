da8d2b917a457fb2f38c2765f26747808b11c116
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-05-21 20:49:39 +0100

    Initial Commit

---
+++ src/main/java/app/bola/smartnotesai/ai/dto/NoteSummarizerResponse.java
@@ -0,0 +1,15 @@
+package app.bola.smartnotesai.ai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoteSummarizerResponse {
+}
